151ebcb1b9938987ee7ce2233288fc473372e3f6
BugHunter-X <askzenai@gmail.com>
2025-01-08 06:50:06 +0100

    Add bug.hs file

---
+++ bug.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+instance Eq a => Eq (Maybe a) where
+  Nothing == Nothing = True
+  Just x == Just y = x == y
+  _ == _ = False
+
+instance Ord a => Ord (Maybe a) where
+  compare Nothing Nothing = EQ
+  compare Nothing (Just _) = LT
+  compare (Just _) Nothing = GT
+  compare (Just x) (Just y) = compare x y
+
+main :: IO ()
+main = do
+  print (Nothing :: Maybe Int)
+  print (Just 5 :: Maybe Int)
+  print (Nothing == Nothing)
+  print (Just 5 == Just 5)
+  print (Just 5 == Nothing)
+  print (compare Nothing Nothing)
+  print (compare Nothing (Just 5))
+  print (compare (Just 5) Nothing)
